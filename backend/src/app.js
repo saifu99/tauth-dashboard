cede94e82c05d345f10fc6b413e6c160dafd32a2
saifu99 <trksaifuyt@gmail.com>
2026-01-21 20:46:10 +0530

    upgrade changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,11 @@ import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
